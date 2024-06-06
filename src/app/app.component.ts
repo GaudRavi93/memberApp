a2dac6447eeae3b49f62ef1e31eba0b33459a20a
Hetal Bhanderi <keyur@disolutions.net>
2024-06-06 16:36:52 +0530

    added log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ import {
   PushNotifications,
   Token,
 } from "@capacitor/push-notifications";
+import { Platform } from '@ionic/angular';
+import * as LiveUpdates from '@capacitor/live-updates';
 
 // Register Swiper
 register();
@@ -25,49 +27,59 @@ export class AppComponent implements OnInit {
   constructor(
     private chatClientService: ChatClientService,
     private chatService: ChatService,
-    private auth: Auth
+    private auth: Auth,
+    private platform: Platform
   ) { }
 
-  async ngOnInit() {
-    // Request permission to use push notifications
-    // iOS will prompt user and return if they granted permission or not
-    // Android will just grant without prompting
-    PushNotifications.requestPermissions().then((result) => {
-      if (result.receive === "granted") {
-        // Register with Apple / Google to receive push via APNS/FCM
-        PushNotifications.register();
-      } else {
-        // Show some error
-        alert("User denied push notification permissions");
+  ngOnInit() {
+    this.platform.ready().then(async () => {
+
+      if(this.platform.is('capacitor')){
+        const resultSync = await LiveUpdates.sync();
+        const resultReload =  await LiveUpdates.reload();
+        console.log('resultReload: ', resultReload);
+        console.log('resultSync: ', resultSync);
+        // Request permission to use push notifications
+        // iOS will prompt user and return if they granted permission or not
+        // Android will just grant without prompting
+        PushNotifications.requestPermissions().then((result) => {
+          if (result.receive === "granted") {
+            // Register with Apple / Google to receive push via APNS/FCM
+            PushNotifications.register();
+          } else {
+            // Show some error
+            console.log("User denied push notification permissions");
+          }
+        });
+    
+        // On success, we should be able to receive notifications
+        PushNotifications.addListener("registration", (token: Token) => {
+          console.log("Push registration success, token: " + token.value);
+          this.chatService.setPushToken(token.value);
+        });
+    
+        // Some issue with our setup and push will not work
+        PushNotifications.addListener("registrationError", (error: any) => {
+          console.log("Error on registration: " + JSON.stringify(error));
+        });
+    
+        // Show us the notification payload if the app is open on our device
+        PushNotifications.addListener(
+          "pushNotificationReceived",
+          (notification: PushNotificationSchema) => {
+            console.log("Push received: " + JSON.stringify(notification));
+          }
+        );
+    
+        // Method called when tapping on a notification
+        PushNotifications.addListener(
+          "pushNotificationActionPerformed",
+          (notification: ActionPerformed) => {
+            console.log("Push action performed: " + JSON.stringify(notification));
+          }
+        );
       }
     });
-
-    // On success, we should be able to receive notifications
-    PushNotifications.addListener("registration", (token: Token) => {
-      alert("Push registration success, token: " + token.value);
-      this.chatService.setPushToken(token.value);
-    });
-
-    // Some issue with our setup and push will not work
-    PushNotifications.addListener("registrationError", (error: any) => {
-      alert("Error on registration: " + JSON.stringify(error));
-    });
-
-    // Show us the notification payload if the app is open on our device
-    PushNotifications.addListener(
-      "pushNotificationReceived",
-      (notification: PushNotificationSchema) => {
-        alert("Push received: " + JSON.stringify(notification));
-      }
-    );
-
-    // Method called when tapping on a notification
-    PushNotifications.addListener(
-      "pushNotificationActionPerformed",
-      (notification: ActionPerformed) => {
-        alert("Push action performed: " + JSON.stringify(notification));
-      }
-    );
   }
 
 }
